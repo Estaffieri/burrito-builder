04ef034c9cd34a6b9590817fd40d3775a27a6769
Estelle Staffieri <estellelareinne@gmail.com>
2021-01-19 12:18:43 -0700

    App test is failing

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -15,7 +15,7 @@ describe("APP TESTS", () => {
             <App />
         )
 
-        const appName = screen.getText("Burrito Builder");
+        const appName = screen.getByText("Burrito Builder");
         expect(appName).toBeInTheDocument();
 
         const nameInput = screen.getByPlaceholderText("Name");
@@ -24,44 +24,41 @@ describe("APP TESTS", () => {
         const order = await waitFor(() => screen.getByText("Estelle"))
         expect(order).toBeInTheDocument();
 
-      
 
     })
 
-    it("should be able to change an input field", () => {
+    it("should display an order after submission", async () => {
+        getOrders.mockResolvedValueOnce({ orders: [{ id: 1, name: "Estelle", ingredients: ["cheese"] }] })
+        postOrder.mockResolvedValueOnce({ orders: [{ id: 2, name: "James", ingredients: ["carnitas", "steak"] }] })
 
         render(
-            <MemoryRouter>
-                <OrderForm />
-            </MemoryRouter>
+            <App />
         )
 
-        const nameInput = screen.getByPlaceholderText("Name");
-        const submitButton = screen.getByText("Submit Order")
-        fireEvent.change(nameInput, { target: { value: "Estelle" } })
-        expect(nameInput.value).toBe("Estelle");
-        expect(submitButton).toBeInTheDocument()
 
-
-    })
-
-
-    it("should be able to clear the input on submit", () => {
-
-        render(
-            <MemoryRouter>
-                <OrderForm />
-            </MemoryRouter>
-        )
-        const mockedFunction = jest.fn();
+        const order = await waitFor(() => screen.getByText("Estelle"))
+        expect(order).toBeInTheDocument();
 
         const nameInput = screen.getByPlaceholderText("Name");
-        fireEvent.change(nameInput, { target: { value: "Estelle" } })
-        expect(nameInput.value).toBe("Estelle");
+        const carnitasButton = screen.getByText("carnitas");
+        expect(carnitasButton).toBeInTheDocument();
+        const steakButton = screen.getByText("steak");
+        expect(steakButton).toBeInTheDocument();
+
+        fireEvent.change(nameInput, {target: {value: "James"}})
+        fireEvent.click(carnitasButton);
+        fireEvent.click(steakButton);
+
         const submitButton = screen.getByText("Submit Order")
         expect(submitButton).toBeInTheDocument()
-        fireEvent.click(submitButton);
-        expect(nameInput.value).toBe("")
+        fireEvent.click(submitButton)
+
+        const newOrder = await waitFor(() => screen.getByText("James"))
+        expect(newOrder).toBeInTheDocument()
+
+        
+
+
 
     })
 
